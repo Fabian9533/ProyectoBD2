c746a308db5c577a3632a00634dc7295850b2838
FMPE <56895239+FMPE@users.noreply.github.com>
2023-05-04 20:52:54 -0500

    final

--- Menus/meta_menu.h
+++ Menus/meta_menu.h
@@ -1,8 +1,7 @@
 #ifndef META_MENU_H
 #define META_MENU_H
-
 #include "../Techniques/DynamicHash/extendibleHash.h"
-#define NOMBRE_ARCHIVO_META "VideojuegosMetacritic.csv"
+#define NOMBRE_ARCHIVO_META "videoGameSales.csv"
 #include <stdio.h>
 #include <time.h>
 
@@ -44,37 +43,38 @@ bool esEntero_r(string linea)
 
     return esEntero;
 }
-void post_insertar_aleatorio_hash(ExtendibleHash<string> &eh);
+void post_insertar_aleatorio_hash(ExtendibleHash<int> &eh);
 
-void insertar_hash(ExtendibleHash<string> &eh)
+void insertar_hash(ExtendibleHash<int> &eh)
 {
 
     ifstream archivo(NOMBRE_ARCHIVO_META);
     string linea;
     clock_t t;
-    char delimitador = ';';
+    char delimitador = ',';
     // Leemos la primea linea para descartarla, pues es el encabezado
     getline(archivo, linea);
-    vector<RecordHash<string>> records;
+    vector<RecordHash<int>> records;
     // Leemos todos las linea
     while (getline(archivo, linea))
     {
         stringstream stream(linea); //Convertir la cadena a un stream
-        string gameName, gamePlatform, releaseDate, gameDeveloper, gameGenre, gamePlayers, gameScore, userScore, gameCritics, gameUsers;
+        string rank, gameName, gamePlatform, gameGenre, gamePublisher, gameYear, naSales, euSales, jpSales, otherSales, globalSales;
         //Extraer todos los valores
-
+        getline(stream, rank, delimitador);
         getline(stream, gameName, delimitador);
         getline(stream, gamePlatform, delimitador);
-        getline(stream, releaseDate, delimitador);
-        getline(stream, gameDeveloper, delimitador);
         getline(stream, gameGenre, delimitador);
-        getline(stream, gamePlayers, delimitador);
-        getline(stream, gameScore, delimitador);
-        getline(stream, userScore, delimitador);
-        getline(stream, gameCritics, delimitador);
-        getline(stream, gameUsers, delimitador);
-       
-        RecordHash<string> temp(gameName, gameName, gamePlatform, releaseDate, gameDeveloper, gameGenre, gamePlayers, stoi(gameScore), stof(userScore), stoi(gameCritics), stoi(gameUsers));
+        getline(stream, gamePublisher, delimitador);
+        getline(stream, gameYear, delimitador);
+        getline(stream, naSales, delimitador);
+        getline(stream, euSales, delimitador);
+        getline(stream, jpSales, delimitador);
+        getline(stream, otherSales, delimitador);
+        getline(stream, globalSales, delimitador);
+        
+        RecordHash<int> temp(stoi(rank), gameName, gamePlatform, gameGenre, gamePublisher, stoi(gameYear), stof(naSales), 
+                             stof(euSales), stof(jpSales), stof(otherSales), stof(globalSales));
         records.push_back(temp);
     }
 
@@ -96,7 +96,7 @@ void post_insertar_aleatorio_hash(ExtendibleHash<int> &eh)
     do
     {
         cout << "\e[1;1H\e[2J";
-        cout << "Se insertaron los datos exitosamente" << endl;
+        cout << "Se insertaron los datos existosamente" << endl;
         cout << endl;
         cout << "Deseas verlos?" << endl;
         cout << "-> Elige un numero" << endl;
@@ -131,39 +131,39 @@ void post_insertar_aleatorio_hash(ExtendibleHash<int> &eh)
     dump();
 }
 
-void insertar_registro_hash(ExtendibleHash<string> &eh)
+void insertar_registro_hash(ExtendibleHash<int> &eh)
 {
     
     cout << "\n\n----------Insertar----------\n\n";
-    int gameScore, gameCritics, gameUsers;
-    string gameName, gamePlatform, releaseDate, gameDeveloper, gameGenre, gamePlayers;
-    float userScore;
+    int rank, gameYear;
+    string gameName, gamePlatform, gameGenre, gamePublisher;
+    float naSales, euSales, jpSales, otherSales, globalSales;
     clock_t t;
-    cout << "Ingrese el nombre: ";
+    cout << "Ingrese el ranking del juego: ";
+    cin >> rank;
+    cout << "Ingrese el nombre del juego[max 80 char]: ";
     cin >> gameName;
-    cout << "Ingrese la plataforma: ";
+    cout << "Ingrese la plataforma del juego[max 20 char]: ";
     cin >> gamePlatform;
-    cout << "Ingrese el dia de lanzamiento: ";
-    cin >> releaseDate;
-    cout << "Ingrese el desarrollador: ";
-    cin >> gameDeveloper;
-    cout << "Ingrese el genero del juego: ";
+    cout << "Ingrese el genero del juego[max 30 char]: ";
     cin >> gameGenre;
-    cout << "Ingrese el estilo de juego(multiplcayer o no/ 1 o dos jugadores): ";
-    cin >> gamePlayers;
-    cout << "Ingrese el puntaje del juego: ";
-    cin >> gameScore;
-    cout << "Ingrese el numero de criticas: ";
-    cin >> gameCritics;
-    cout << "Ingrese el numero de usuarios: ";
-    cin >> gameUsers;
-    cout << "Ingrese el score del campeon: ";
-    cin >> userScore;
-    cout << "Ingrese el numero de usuarios: ";
+    cout << "Ingrese el publisher del juego[max 30 char]: ";
+    cin >> gamePublisher;
+    cout << "Ingrese el anho de publicacion del juego: ";
+    cin >> gameYear;
+    cout << "Ingrese sus ventas en Norte America: ";
+    cin >> naSales;
+    cout << "Ingrese sus ventas en Europa: ";
+    cin >> euSales;
+    cout << "Ingrese sus ventas en japon: ";
+    cin >> jpSales;
+    cout << "Ingrese sus otras ventas: ";
+    cin >> otherSales;
+    cout << "Ingrese sus ventas globales: ";
+    cin >> globalSales;
+    RecordHash<int> temp(rank, gameName, gamePlatform, gameGenre, gamePublisher, gameYear, naSales,  euSales,  jpSales,  otherSales,  globalSales);
 
-    RecordHash<string> temp(gameName, gameName, gamePlatform, releaseDate, gameDeveloper, gameGenre, gamePlayers, gameScore, userScore, gameCritics, gameUsers);
-
-    RecordHash<string> rec(gameName, gameName, gamePlatform, releaseDate, gameDeveloper, gameGenre, gamePlayers, gameScore, userScore, gameCritics, gameUsers);
+    RecordHash<int> rec(rank, gameName, gamePlatform, gameGenre, gamePublisher, gameYear, naSales,  euSales,  jpSales,  otherSales,  globalSales);
     int accesos = 1;
     t = clock();
     eh.insert(rec, accesos);
